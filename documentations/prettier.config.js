bdd4dc5a9c4259298b2a5318f3c85b30886582e7
Muhammad Asif <96514685+mdasif-join@users.noreply.github.com>
2025-08-27 11:37:05 +0600

    Add Prettier configuration file

---
+++ documentations/prettier.config.js
@@ -0,0 +1,8 @@
+export default {
+  plugins: ["prettier-plugin-tailwindcss"], // Auto-sort Tailwind classes
+  semi: true, // Always add semicolons
+  singleQuote: false, // Use double quotes instead of single quotes
+  trailingComma: "es5", // Add trailing commas where valid in ES5 (objects, arrays, etc.)
+  tabWidth: 2, // 2 spaces per tab
+  printWidth: 100, // Wrap lines at 100 characters
+};
